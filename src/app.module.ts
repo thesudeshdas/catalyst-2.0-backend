ba17a3bad22ffaec1f43ef8ed7c506a73c9e5bfb
thesudeshdas <sudeshkumardas7@gmail.com>
2023-12-14 16:04:07 +0530

    feat: mongoDB setup, users route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
-import { Module } from '@nestjs/common';
+import { AppConfigurationModule } from './infrastructure/configuration/app-configuration.module';
+import { AppConfigurationService } from './infrastructure/configuration/app-configuration.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
+import { RouterModule } from '@nestjs/core';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AppConfigurationModule,
+    MongooseModule.forRootAsync({
+      imports: [AppConfigurationModule],
+      inject: [AppConfigurationService],
+      useFactory: (appConfigService: AppConfigurationService) => {
+        const options: MongooseModuleOptions = {
+          uri: appConfigService.connectionString,
+        };
+        return options;
+      },
+    }),
+    RouterModule.register([
+      {
+        path: 'user',
+        module: UserModule,
+      },
+    ]),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './user.schema';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+
+@Module({
+  controllers: [UserController],
+  exports: [],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UserSchema,
+      },
+    ]),
+  ],
+  providers: [UserService],
+})
+export class UserModule {}
